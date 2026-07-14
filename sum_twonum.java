e5b0742d53eb4ddbac894af68ab5f07345c93b5b
anshi-io <anshimalik64@gmail.com>
2026-07-14 14:40:10 +0530

    two number sum

---
+++ sum_twonum.java
@@ -0,0 +1,13 @@
+//sum of two numbers
+import java.util.*;
+public class sum_twonum {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum = 0;
+        sum= a+b;
+        System.out.println(sum);
+
+    }
+}
