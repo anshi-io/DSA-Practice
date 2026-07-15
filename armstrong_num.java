9833682b11894e5c5222cd9ac0cd2108eaac6755
anshi-io <anshimalik64@gmail.com>
2026-07-15 20:44:33 +0530

    Armstrong num

---
+++ armstrong_num.java
@@ -0,0 +1,9 @@
+//armstrong number
+import java.util.*;
+public class armstrong_num {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        
+    }
+}
