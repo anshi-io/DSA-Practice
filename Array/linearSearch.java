71e8e170b2c493b4a58efa22697053c1b7ea623f
Anshi <anshimalik64@gmail.com>
2026-08-15 20:13:48 +0530

    linear Search

--- Array/linearSearch.java
+++ Array/linearSearch.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class linearSearch{
+   //linear search
+
+   public static int linearSearch(int numArr[],int key){
+
+      for(int i=0;i<=numArr.length;i++){
+         if(numArr[i]==key){
+            return i;
+         }
+         
+      }
+      return -1;
+   }
+
    public static void main(String[] args) {
-    //linear search
+       int arr_ex[]={2,5,7,9,32,76};
+       Scanner sc = new Scanner(System.in);
+       int key = sc.nextInt();
+       int linear_search_index=linearSearch(arr_ex, key);
+       System.out.println(linear_search_index);
+       
+    
    }
 }
