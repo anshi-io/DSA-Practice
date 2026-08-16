8a5bfe2b1547bbccf357b1147eb497da969a3be7
Anshi <anshimalik64@gmail.com>
2026-08-16 23:26:51 +0530

    to print Subarray

---
+++ Array/binarySearch.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class binarySearch {
+   public static int binarySearch_arr(int numArr[]){
+    
+   }
+  
+}
---
+++ Array/printSubarray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class printSubarray {
+  public static void getsubArr(int numArr[]){
+      for (int i=0;i<numArr.length;i++){
+        for(int j=i;j<numArr.length;j++){//we want element as a single subArray
+          for(int k = i;k<j;k++){
+            System.out.print(numArr[k]);
+          }
+          System.out.println();
+        }
+        System.out.println();
+      }
+      
+  }
+
+  public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    int numbArr[]=new int[5];
+    for(int i=0;i<numbArr.length;i++){
+      numbArr[i]=sc.nextInt();
+    }
+    
+    getsubArr(numbArr);
+  }
+}
