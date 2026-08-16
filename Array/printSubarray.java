8455e063beb3fc5404bd745e545e64d431fd2051
Anshi <anshimalik64@gmail.com>
2026-08-16 23:35:00 +0530

    total subarrays

--- Array/printSubarray.java
+++ Array/printSubarray.java
@@ -1,15 +1,18 @@
 import java.util.*;
 public class printSubarray {
   public static void getsubArr(int numArr[]){
+    int totalSubarr=0;//to count total subarrays
       for (int i=0;i<numArr.length;i++){
         for(int j=i;j<numArr.length;j++){//we want element as a single subArray
-          for(int k = i;k<j;k++){
+          for(int k = i;k<=j;k++){
             System.out.print(numArr[k]);
           }
+          totalSubarr++;
           System.out.println();
         }
         System.out.println();
       }
+      System.out.println("Total subarrays of the array is:"+totalSubarr);
       
   }
 
